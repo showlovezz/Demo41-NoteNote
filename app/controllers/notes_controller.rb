13ec736f711deaa9c274b4fbf3d824152f160481
vita <dragic.hsieh@gmail.com>
2018-06-26 16:54:56 +0800

    Done note generate model and pages

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,51 @@
+class NotesController < ApplicationController
+
+	before_action :find_note, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@notes = Note.order("created_at DESC")
+	end
+
+	def new
+		@note = Note.new
+	end
+
+	def create
+		@note = Note.new(note_params)
+		if @note.save
+			redirect_to notes_path, notice: "記事本已建立"
+		else
+			render "new", alert: "記事本建立失敗，再檢查一下吧!"
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @note.update(note_params)
+			redirect_to notes_path, notice: "記事本修改成功"
+		else
+			render "edit", alert: "記事本修改失敗!"
+		end
+	end
+
+	def destroy
+		@note.destroy
+		redirect_to notes_path, alert: "文章已被刪除"
+	end
+
+	private
+
+	def note_params
+		params.require(:note).permit(:title, :content)
+	end
+
+	def find_note
+		@note = Note.find(params[:id])
+	end
+
+end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,4 @@
 class Note < ApplicationRecord
+	validates :title, length: {minimum: 3}
+	validates :content, presence: true
 end
